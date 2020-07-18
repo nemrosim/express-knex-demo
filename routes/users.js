844af5a965fccf8686ed4e14b597971ded8ff4b5
Artem Diashkin <nemrosim1988@gmail.com>
2020-07-18 22:59:32 +0300

    add joins and wheres

--- routes/users.js
+++ routes/users.js
@@ -33,10 +33,20 @@ usersRouter.route('/whereNot')
 
 usersRouter.route('/whereIn')
     .get(async (request, response) => {
-        const users = await knex('users')
-            .whereNot(request.query)
-            .select('first_name', 'last_name')
-        response.json({users});
+        const {age} = request.query;
+
+        const users = await knex('users').whereIn('age', age);
+
+        response.json({users: users});
+    });
+
+usersRouter.route('/whereBetween')
+    .get(async (request, response) => {
+        const {age} = request.query;
+
+        const users = await knex('users').whereBetween('age', age);
+
+        response.json({users: users});
     });
 
 usersRouter.route('/joinWithProfiles')
@@ -44,6 +54,22 @@ usersRouter.route('/joinWithProfiles')
         const users = await knex('users')
             .join('profiles', 'users.id', '=', 'profiles.user_id')
             .select("users.first_name", 'profiles.email')
+        response.json({users});
+    });
+
+usersRouter.route('/innerJoin')
+    .get(async (request, response) => {
+        const users = await knex('users')
+            .innerJoin('profiles', 'users.id', '=', 'profiles.user_id')
+            .select("users.first_name", 'profiles.email')
+        response.json({users});
+    });
+
+usersRouter.route('/leftJoin')
+    .get(async (request, response) => {
+        const users = await knex('users')
+            .leftOuterJoin('orders', 'users.id', '=', 'orders.user_id')
+            .select("users.id", "users.first_name", 'users.last_name', 'orders.id AS order')
         response.json({users});
     });
 
